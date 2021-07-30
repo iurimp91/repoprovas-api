a032d97e00c8e4348a1cd5d82138c74f819f4b26
Iuri Magnago <iurimp91@gmail.com>
2021-07-30 00:35:39 -0300

    Fixed year joi validation

--- src/validations/examValidation.ts
+++ src/validations/examValidation.ts
@@ -1,11 +1,13 @@
 import ExamInterface from "../interfaces/ExamInterface";
 import joi from "joi";
+import dayjs from "dayjs";
 
+const currentYear = dayjs().year();
 const linkRegEx = /^http(s?)\:\/\/[0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*(:(0-9)*)*(\/?)([a-zA-Z0-9\-\.\?\,\'\/\\\+&amp;%\$#_]*)?(.pdf)$/;
 
 export async function examValidation(exam: ExamInterface): Promise<ExamInterface> {
     const schema = joi.object({
-        year: joi.number().integer().positive().required(),
+        year: joi.number().integer().min(2000).max(currentYear).required(),
         semester: joi.number().integer().min(1).max(2).required(),
         categoryId: joi.number().integer().min(1).required(),
         subjectId: joi.number().integer().min(1).required(),
