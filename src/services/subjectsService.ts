b61ef35a3192327a9cd83f0673d9d3013a6a77b2
Iuri Magnago <iurimp91@gmail.com>
2021-08-01 19:35:32 -0300

    Get subjects route now send exams list

--- src/services/subjectsService.ts
+++ src/services/subjectsService.ts
@@ -3,12 +3,13 @@ import { getRepository } from "typeorm";
 import Subjects from "../entities/Subjects"
 import { SubjectsInterface } from "../interfaces/SubjectsInterface";
  
-async function findSubjects(): Promise<SubjectsInterface[]> {
+async function findSubjects(): Promise<SubjectsInterface[]> {        
     const result: SubjectsInterface[] = await getRepository(Subjects)
         .createQueryBuilder("subjects")
+        .leftJoinAndSelect("subjects.exams", "exams")
         .orderBy("subjects.name")
         .getMany();
-        
+
     return result;
 }
 
