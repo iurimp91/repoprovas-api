b031ce9f7d8ec4932e09fb7b9ebbc425b6e10963
leleoRA <celso@respondeai.com.br>
2021-08-09 14:44:56 -0300

    Preparando revisão de código

--- src/interfaces/SubjectsInterface.ts
+++
@@ -1,4 +0,0 @@
-export interface SubjectsInterface {
-    name: string,
-    period: string,
-}
--- src/setup.ts
+++
@@ -1,7 +0,0 @@
-import dotenv from "dotenv";
-
-const envFile = process.env.NODE_ENV === "test"
-    ? ".env.test"
-    : (process.env.NODE_ENV === "development" ? ".env.dev" : ".env");
-
-dotenv.config({ path: envFile });
