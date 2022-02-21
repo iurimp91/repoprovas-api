13bd926c973c42f4bdc36ba387a5f0cec544bbe1
Iuri Magnago Pinto <iurimp91@gmail.com>
2022-02-21 12:01:45 -0300

    change entities path in database file

--- src/database.ts
+++ src/database.ts
@@ -10,7 +10,7 @@ export default async function connectDatabase() {
         name: "default",
         type: "postgres",
         url: process.env.DATABASE_URL,
-        entities: [`${process.env.NODE_ENV === 'production' ? 'dist' : 'src'}/entities/**/*{.ts,.js}`],
+        entities: [`${process.env.NODE_ENV === 'production' ? 'dist' : 'src'}/entities/**/*.*`],
         ssl: process.env.NODE_ENV === 'production'
     });
 
