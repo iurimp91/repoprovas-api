e104ff7f5d3cc3ee1c9fb591142beac882e62835
Iuri Magnago Pinto <iurimp91@gmail.com>
2022-02-21 13:29:52 -0300

    change entities path in database file

--- src/database.ts
+++ src/database.ts
@@ -4,13 +4,15 @@ if (process.env.NODE_ENV === 'production' && process.env.DATABASE_URL.indexOf("s
     process.env.DATABASE_URL += "?sslmode=require";
 }
 
+console.log(`${process.env.NODE_ENV === 'production' ? 'dist' : 'src'}/entities/*.*`);
+
 export default async function connectDatabase() {
     const connectionManager = await getConnectionManager();
     const connection = connectionManager.create({
         name: "default",
         type: "postgres",
-        url: process.env.DATABASE_URL,
-        entities: ["__dirname + '/entities/*.*"],
+        url: "postgres://postgres:123456@localhost:5432/repoprovas_test",
+        entities: [`${process.env.NODE_ENV === 'production' ? 'dist' : 'src'}/entities/*.*`],
         ssl: process.env.NODE_ENV === 'production'
     });
 
